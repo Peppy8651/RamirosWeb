1c4ac2c92edfc6cd1a0d10ee556c4bad488b3bf0
Peppy8651 <73673342+Peppy8651@users.noreply.github.com>
2026-04-03 19:36:26 -0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // This ensures paths point to https://peppy8651.github.io/RamirosWeb/ 
+  // instead of the root domain.
+  base: '/RamirosWeb/', 
+})
